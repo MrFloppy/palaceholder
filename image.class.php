a2ab49945778b00fa59d9ea9507099f3e9c4c25d
Maximilian Sohrt <max@sohrt.de>
2013-02-11 19:13:45 +0100

    Initial commit

---
+++ image.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of image
+ *
+ * @author max
+ */
+class image {
+    //put your code here
+}
+
+?>
