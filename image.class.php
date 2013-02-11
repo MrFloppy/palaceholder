95140e90ccf1978f72713782aca8dccf54dbabee
Maximilian Sohrt <max@sohrt.de>
2013-02-11 23:17:47 +0100

    Finished plain image creation

--- image.class.php
+++ image.class.php
@@ -19,20 +19,19 @@ class image {
    */
   private $_width = 0;
   private $_height = 0;
-  private $_color = 0x00FF0000;
+  private $_color;
 
   /**
    * 
    * @param type $height
    * @param type $width
    */
-  public function __construct($height, $width, $color) {
+  public function __construct($height, $width, $color = "#FFCC00") {
     if (($height != 0) && ($width != 0)) {
       $this->setHeight($width);
       $this->setWidth($width);
-      
-      $this->setColor("0x00" . $color);
-      
+      $this->setColor($color);
+          
       $this->createImage();
     } else {
       die("Nothing to do here. No parameters, no work!");
@@ -54,14 +53,34 @@ class image {
   }
   
   private function setColor($color) {
-    $this->_color = (int) $color;
+    $this->_color = $color;
+  }
+  
+  private function hexToRgb($hex) {
+    
+   $hex = str_replace("#", "", $hex);
+
+   if(strlen($hex) == 3) {
+      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+   } else {
+      $r = hexdec(substr($hex,0,2));
+      $g = hexdec(substr($hex,2,2));
+      $b = hexdec(substr($hex,4,2));
+   }
+   $rgb = array($r, $g, $b);
+   //return implode(",", $rgb); // returns the rgb values separated by commas
+   return $rgb; // returns an array with the rgb values
   }
 
   public function createImage() {
     if (($this->_height != 0) && ($this->_width != 0)) {
       header('Content-Type: image/png');
       $im = imagecreatetruecolor($this->_width, $this->_height);
-      imagefilledrectangle($im, 0, 0, $this->_width, $this->_height, $this->_color);
+      $color = $this->hexToRgb($this->_color);
+      $color = imagecolorallocate($im, $color[0], $color[1], $color[2]);
+      imagefilledrectangle($im, 0, 0, $this->_width, $this->_height, $color);
       imagepng($im);
       imagedestroy($im);
     } else {
--- index.php
+++ index.php
@@ -9,10 +9,16 @@ include("image.class.php");
 $height = (int) $_GET['height'];
 $width = (int) $_GET['width'];
 
+
 //var_dump($height);
 //echo "<br>";
 //var_dump($width);
+if (isset($_GET['color'])) {
+  $color = $_GET['color'];
+  $image = new image($height, $width, $color);
+} else {
+  $image = new image($height, $width);  
+}
 
-$image = new image($height, $width);
 
 ?>
